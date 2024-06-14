dea9633a427f73aee3e81bb4734ac6e5fab76e5e
gumeeee <gaxco123@gmail.com>
2024-06-14 01:36:37 -0300

    feat - rota de invite implementada com funcionalidade de criação de usuário

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,13 +1,14 @@
 const UserLoginService = require("../useCases/UserLoginService");
 const userLoginService = new UserLoginService();
+
 const UserListService = require("../useCases/UserListService");
 const userListService = new UserListService();
+
 const UserRegisterService = require("../useCases/UserRegisterService");
 const userRegisterService = new UserRegisterService();
 
-const jwt = require("jsonwebtoken");
-
-const bcrypt = require("bcryptjs");
+const UserInviteService = require("../useCases/UserInviteService");
+const userInviteService = new UserInviteService();
 
 module.exports = {
   async index(req, res) {
@@ -39,42 +40,12 @@ module.exports = {
   },
 
   async inviteMember(req, res, next) {
-
-    /*
-    const { email, name } = req.body;
-
-    if (!email || !name) {
-      throw new InvalidCredentialsError("Email e Nome são obrigatórios.");
-    }
-
-    if (!token) {
-      return res.status(401).json({ message: "Token não informado." });
-    }
-
-    let token = req.headers.authorization;
-
     try {
-      token = token.split(" ")[1];
-      const payload = await jwt.verify(token, process.env.SECRET_KEY);
+      await userInviteService.execute(req);
 
-      const adminCompany = payload.companyId;
-      
-      console.log(req.body.email);
-
-      const newUser = {
-        email,
-        password: await bcrypt.hash("senha_temporaria", 10),
-        name,
-        companyId: Number(adminCompany),
-        roleId: 2,
-      };
-
-
-      res.status(200).json({ message: "Usuário convidado com sucesso." });
+      res.status(201).json({message: "Usuário MEMBER criado!"})
     } catch (err) {
-      res.status().json({ message: err.message });
-      next();
+      res.status(400).json({message: err.message});
     }
-    */
   },
 };
--- src/useCases/UserInviteService.js
+++ src/useCases/UserInviteService.js
@@ -1,11 +1,35 @@
-const bcrypt = require("bcryptjs");
-const GenerateJwtTokenProvider = require("../providers/GenerateJwtTokenProvider");
-const InvalidCredentialsError = require("../errors/InvalidCredentialsError");
+const jwt = require("jsonwebtoken");
 
-const generateJwtTokenProvider = new GenerateJwtTokenProvider();
+const bcrypt = require("bcryptjs");
+const UserRepository = require("../repository/UserRepository");
+const userRepository = new UserRepository();
 
 class UserInviteService {
   async execute(params) {
-  
+    let token = params.headers.authorization;
+
+    token = token.split(" ")[1];
+
+    const payload = await jwt.verify(token, process.env.SECRET_KEY);
+
+    const { email, name } = params.body;
+
+    if (!email || !name) {
+      throw new InvalidCredentialsError("Email e Nome são obrigatórios.");
+    }
+
+    const adminCompany = Number(payload.companyId);
+
+    const newUser = {
+      email,
+      password: await bcrypt.hash("senha_temporaria", 10),
+      name,
+      company_id: adminCompany,
+      role_id: 2,
+    };
+
+    return userRepository.createUser(newUser);
   }
 }
+
+module.exports = UserInviteService;
