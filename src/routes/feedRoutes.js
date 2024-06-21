d7b11855ec2acad4d3451d0097142099d250a982
gumeeee <gaxco123@gmail.com>
2024-06-21 16:25:51 -0300

    chore: removing comments in feedRoutes and rename variables names

--- src/routes/feedRoutes.js
+++ src/routes/feedRoutes.js
@@ -12,14 +12,11 @@ const storage = multer.diskStorage({
     cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
-    // Extração da extensão do arquivo original:
-    const extensaoArquivo = file.originalname.split(".")[1];
+    const fileExtension = file.originalname.split(".")[1];
 
-    // Cria um código randômico que será o nome do arquivo
-    const novoNomeArquivo = require("crypto").randomBytes(64).toString("hex");
+    const newNameFile = require("crypto").randomBytes(64).toString("hex");
 
-    // Indica o novo nome do arquivo:
-    cb(null, `${novoNomeArquivo}.${extensaoArquivo}`);
+    cb(null, `${newNameFile}.${fileExtension}`);
   },
 });
 
